0c114215e80e77c53287e850a712095b80d206b4
Firas-Amin <mr_firas2012@hotmail.com>
2021-05-22 05:12:37 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(TimeTracker());
+}
+
+class TimeTracker extends StatelessWidget {
+  const TimeTracker({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
